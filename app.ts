50bdeb2533a332f7edf7667602579e84435d1878
Tiago Gomes <tidgomes@gmail.com>
2023-03-15 09:45:39 -0300

    undestandig type and interfaces

--- app.ts
+++ app.ts
@@ -5,14 +5,22 @@ const buttonElement = document.querySelector('button')!
 const numResults: number[] = []
 const textResults: string[] = []
 
-function add(num1: number | string, num2: number | string) {
+type NumOrString = number | string
+type Result = {val: number; timestamp: Date}
+
+interface ResultObj {
+  val: number
+  timestamp: Date
+}
+
+function add(num1: NumOrString, num2: NumOrString) {
   if (typeof num1 === 'number' && typeof num2 === 'number') return num1 + num2
   else if (typeof num1 === 'string' && typeof num2 === 'string')
     return num1 + ' ' + num2
   return +num1 + +num2
 }
 
-function printResult(resultObj: { val: number; timestamp: Date }) {
+function printResult(resultObj: Result) {
   console.log(resultObj.val)
 }
 
