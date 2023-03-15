c1f47feecfe1ff67e2055dd6274e4e3050da0919
Tiago Gomes <tidgomes@gmail.com>
2023-03-15 10:39:12 -0300

    create route to handle requests to edit a todo

--- routes/todos.ts
+++ routes/todos.ts
@@ -16,6 +16,20 @@ router.post('/todo', (req, res, next) => {
   }
 
   todos.push(todo)
+  res.status(201).json({ message: 'Todo created', todo, todos })
+})
+
+router.put('/post/:postId', (req, res, next) => {
+  const tid = req.params.postId
+  const text = req.body.text
+
+  const todoIndex = todos.findIndex(todoItem => todoItem.id === tid)
+  if (todoIndex >= 0) {
+    todos[todoIndex] = { id: todos[todoIndex].id, text }
+    return res.status(200).json({ message: 'Todo edited' })
+  }
+
+  res.status(404).json({ message: 'Todo item not found' })
 })
 
 export default router
